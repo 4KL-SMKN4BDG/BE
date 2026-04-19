340327b1105c5d04e57b15de4fdbb8f152f59d17
CodeVin0 <kevinalfarizi428@gmail.com>
2026-04-19 19:05:50 +0700

    solve some error, idk why it's solved

--- src/helpers/convertGmapsURL.helper.ts
+++ src/helpers/convertGmapsURL.helper.ts
@@ -8,21 +8,20 @@ export async function convertGmapsUrl(url: string) {
 
         const finalUrl = res.request.res.responseUrl;
 
-        const match = finalUrl.match(/@(-?\d+\.\d+),(-?\d+\.\d+),(\d+(\.\d+)?)z/);
+        const placeMatch = finalUrl.match(/place\/([^/]+)/);
+        if (placeMatch) {
+            const place = decodeURIComponent(placeMatch[1]);
+            
+            return `https://www.google.com/maps?q=${place}&z=15&output=embed`;
+        }
 
+        const match = finalUrl.match(/@(-?\d+\.\d+),(-?\d+\.\d+),(\d+(\.\d+)?)z/);
         if (match) {
             const lat = match[1];
             const lng = match[2];
             const zoom = Math.round(parseFloat(match[3]));
 
             return `https://www.google.com/maps?q=${lat},${lng}&z=${zoom}&output=embed`;
-        }
-
-        const placeMatch = finalUrl.match(/place\/([^/]+)/);
-        if (placeMatch) {
-            const place = decodeURIComponent(placeMatch[1]);
-            
-            return `https://www.google.com/maps?q=${place}&z=15&output=embed`;
         }
 
         return null;
